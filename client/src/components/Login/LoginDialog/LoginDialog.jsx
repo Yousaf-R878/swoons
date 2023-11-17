e7fcde268004978186929870d62e3a33a0a3d48c
semphoenix <rodriguezs2021@gmail.com>
2023-11-16 21:05:59 -0500

    fixed merge issues with login

--- client/src/components/Login/LoginDialog/LoginDialog.jsx
+++ client/src/components/Login/LoginDialog/LoginDialog.jsx
@@ -1,10 +1,12 @@
-
 import {
   DialogContent,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import LoginForm from "../LoginForm/LoginForm";
+import { Button } from "@/components/ui/button";
+import { signInWithPopup, GoogleAuthProvider, getAuth } from "firebase/auth";
+import "./LoginDialog.css";
 
 const LoginDialog = () => {
   // 2. Define a submit handler.
@@ -13,11 +15,26 @@ const LoginDialog = () => {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+  async function handleGoogle() {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    const userCred = await signInWithPopup(auth, provider);
+    console.log(userCred);
+    if (userCred.user) {
+      console.log("navigate or smn");
+    }
+  }
   return (
     <DialogContent className="sm:max-w-[425px]">
       <DialogHeader>
         <DialogTitle className="text-2xl text-center">Login</DialogTitle>
       </DialogHeader>
+      <Button
+        onClick={handleGoogle}
+        className="googleButton gradient-border bg-white py-2 px-4 cursor-pointer font-bold hover:bg-gray-100"
+      >
+        <span className="googleText">Log in with Google</span>
+      </Button>
       <div className="flex items-center my-4">
         <div className="flex-grow border-t border-gray-300"></div>
         <span className="mx-4 text-gray-600">OR</span>
