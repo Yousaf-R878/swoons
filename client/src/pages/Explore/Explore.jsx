bee296414d074c116161985f5c1113b964b3dbad
notalim <alimneedstowork@gmail.com>
2023-11-25 15:57:01 -0500

    explore page basic outline

--- client/src/pages/Explore/Explore.jsx
+++ client/src/pages/Explore/Explore.jsx
@@ -1,15 +1,56 @@
-import Navbar from "@/src/components/Navbar/Navbar";
-import PostCard from "@/src/components/PostCard/PostCard";
-import React from "react";
+import React, { useState } from "react";
 import NavbarExplore from "../../components/Navbar/NavbarExplore";
-
-import { Input, Badge, Select } from "@/components/ui/input";
+import PostCard from "@/src/components/PostCard/PostCard";
+import { Input } from "@/components/ui/input";
+import { Badge } from "@/components/ui/badge";
+import { Select } from "@/components/ui/select";
 
 const Explore = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [badges, setBadges] = useState([]);
+    const [selectedSort, setSelectedSort] = useState("recent");
+
+    // Function to handle the input change
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    // * This handles ENTER PRESS
+    const handleInputKeyPress = (e) => {
+        if (e.key === "Enter" && inputValue) {
+            setBadges([...badges, inputValue]);
+            setInputValue(""); 
+        }
+    };
+
+    const handleSortChange = (e) => {
+        setSelectedSort(e.target.value);
+    };
+
     return (
         <>
             <NavbarExplore />
-            <Input placeholder="Search for dates!" />
+            <div className="container mx-auto p-4">
+                <Input
+                    placeholder="Search for dates!"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onKeyPress={handleInputKeyPress}
+                />
+                <div className="flex flex-wrap gap-2 my-4 ">
+                    {badges.map((badge, index) => (
+                        <Badge className="bg-palecyan " key={index}>{badge}</Badge>
+                    ))}
+                </div>
+                <Select value={selectedSort} onChange={handleSortChange}>
+                    <option value="recent">Recent</option>
+                    <option value="popular">Popular</option>
+                </Select>
+                <div className="grid grid-cols-3 gap-4">                    <PostCard />
+                    <PostCard />
+                    <PostCard />
+                </div>
+            </div>
         </>
     );
 };
