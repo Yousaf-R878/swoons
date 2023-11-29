1c678b87a4d0bab2e37ecbba700c66253b4c1637
khalton55 <117873588+khalton55@users.noreply.github.com>
2023-11-29 18:43:17 -0500

    Update seed.js

--- server/src/tasks/seed.js
+++ server/src/tasks/seed.js
@@ -6,52 +6,16 @@ const db = await dbConnection();
 
 let main = async () => {
     try{
-        let user = await userFuncs.create("Kyle","Halton","kyle@kyle.com","Password123!");
-        console.log(user);
-
-        let getUser = await userFuncs.get(user._id);
-        console.log(getUser);
-        //let getUserError = await userFuncs.get('656144459cea04fc28ad33a2')
-        //Test ID (Doesnt exist): '656144459cea04fc28ad33a2'
+        let user1 = await userFuncs.create("Kyle","Halton","kyle@kyle.com","Password123!");
+        let likedPost = await userFuncs.addLikedDate(user1._id, '656144459cea04fc28ad33a2');
+        let addDate = await userFuncs.addDate(user1._id, '656144459cea04fc28ad33a2');
 
         let user2 = await userFuncs.create("Michael", "Michaelton", "michael@michael.com", "Password456!");
-
-        // let getAll = await userFuncs.getAll();
-        // console.log(getAll);
-
-        let updateUser2 = await userFuncs.update(user2._id,"Mike","Miketon","mike@mike.com","Mike123!");
-        console.log(updateUser2);
-
-        //let removeUser = await userFuncs.remove(user2._id);
-
-        //let getRemove = await userFuncs.get(user2._id);
-
-        let addPost = await userFuncs.addPost(user2._id, '656144459cea04fc28ad33a2');
-        console.log(addPost);
-
-        let removePost = await userFuncs.removePost(user2._id,'656144459cea04fc28ad33a2');
-        console.log(removePost)
-
-        let addDate = await userFuncs.addDate(user2._id,'656144459cea04fc28ad33a2');
-        console.log(addDate);
-
-        let removeDate = await userFuncs.removeDate(user2._id,'656144459cea04fc28ad33a2');
-        console.log(removeDate);
 
     } catch (e){
         console.log(e);
     }
-
 };
 
-
-
-
-
-
-
-
-
 await main();
-await db.dropDatabase();
 await closeConnection();
