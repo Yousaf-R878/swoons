3c373f12bc9c5c2ef4d48775ac4e06e8a4ba1c30
khalton55 <117873588+khalton55@users.noreply.github.com>
2023-11-30 19:33:22 -0500

    Update users.js

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,142 +1,215 @@
-import {Router} from 'express';
+import { Router } from "express";
 const router = Router();
-import * as userFuncs from '../data/users.js'
-import * as helpers from '../data/helpers.js'
-import { users } from '../config/mongoCollections.js';
+import * as userFuncs from "../data/users.js";
+import * as helpers from "../data/helpers.js";
+import { users } from "../config/mongoCollections.js";
+
+router.route("/").get(async (req, res) => {
+  try {
+    let allUsers = await userFuncs.getAll();
+    return res.status(200).json(allUsers);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
+
+router.route("/signup").post(async (req, res) => {
+  let userInfo = req.body;
+  if (!userInfo || Object.keys(userInfo).length === 0) {
+    return res
+      .status(400)
+      .json({ error: "There are no fields in the request body" });
+  }
+
+  let firstName = userInfo.firstName;
+  let lastName = userInfo.lastName;
+  let email = userInfo.email;
+  let password = userInfo.password;
+
+  try {
+    //VALIDATE FIELDS
+    firstName = helpers.checkName(firstName, "First Name");
+    lastName = helpers.checkName(lastName, "Last Name");
+    email = helpers.checkEmail(email, "Email");
+    password = helpers.checkPassword(password, "Password");
+
+    const usersCollection = await users();
+    let userExists = await usersCollection.findOne({ email: email });
+    if (userExists) {
+      throw `User with that email (${email}) already exists!`;
+    }
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
+
+  try {
+    let newUser = await userFuncs.create(firstName, lastName, email, password);
+    return res.status(200).json(newUser);
+  } catch (e) {
+    return res.status(500).json({ error: e });
+  }
+});
 
 router
-    .route('/')
-    .get(async (req, res) => {
-        try{
-            let allUsers = await userFuncs.getAll();
-            return res.status(200).json(allUsers)
-        } catch (e){
-            return res.status(500).json({error: e})
-        }
-    })
+  .route("/:id")
+  .get(async (req, res) => {
+    let id = req.params.id;
+
+    try {
+      id = helpers.checkId(id, "ID");
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+
+    try {
+      let user = await userFuncs.get(id);
+      return res.status(200).json(user);
+    } catch (e) {
+      if (e === `No User with that ID (${id})`) {
+        return res.status(404).json({ error: e });
+      } else {
+        return res.status(500).json({ error: e });
+      }
+    }
+  })
+  .patch(async (req, res) => {
+    let userInfo = req.body;
+    let id = req.params.id;
+
+    try {
+      id = helpers.checkId(id, "ID");
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+
+    const usersCollection = await users();
+    let user;
+
+    try {
+      user = await userFuncs.get(id);
+    } catch (e) {
+      return res.status(404).json({ error: e });
+    }
+
+    if (!userInfo || Object.keys(userInfo).length === 0) {
+      return res
+        .status(400)
+        .json({ error: "There are no fields in the request body" });
+    }
+
+    let firstName = userInfo.firstName;
+    let lastName = userInfo.lastName;
+    let email = userInfo.email;
+    let password = userInfo.password;
+
+    if (!firstName) {
+      firstName = user.firstName;
+    }
+
+    if (!lastName) {
+      lastName = user.lastName;
+    }
+
+    if (!email) {
+      email = user.email;
+    }
+
+    if (!password) {
+      password = user.password;
+    }
+
+    try {
+      firstName = helpers.checkName(firstName, "First Name");
+      lastName = helpers.checkName(lastName, "Last Name");
+      email = helpers.checkEmail(email, "Email");
+      password = helpers.checkPassword(password, "Password");
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+
+    try {
+      let updatedUser = await userFuncs.update(
+        id,
+        firstName,
+        lastName,
+        email,
+        password
+      );
+      return res.status(200).json(updatedUser);
+    } catch (e) {
+      return res.status(500).json({ error: e });
+    }
+  })
+  .delete(async (req, res) => {
+    let id = req.params.id;
+
+    try {
+      id = helpers.checkId(id, "ID");
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+
+    try {
+      let user = await userFuncs.get(id);
+    } catch (e) {
+      if (e === `No User with that ID (${id})`) {
+        return res.status(404).json({ error: e });
+      } else {
+        return res.status(500).json({ error: e });
+      }
+    }
+
+    try {
+      let deletedUser = await userFuncs.remove(id);
+      return res.status(200).json(deletedUser);
+    } catch (e) {
+      return res.status(500).json({ error: e });
+    }
+  });
 
 router
-    .route('/signup')
-    .post(async (req, res) => {
-        let userInfo = req.body;
-        if (!userInfo || Object.keys(userInfo).length === 0){
-            return res.status(400).json({error: 'There are no fields in the request body'});
-        }
+  .route("/:userId/like/:dateId")
+  .post(async (req, res) => {
+    let userId = req.params.userId;
+    let dateId = req.params.dateId;
 
-        let firstName = userInfo.firstName;
-        let lastName = userInfo.lastName;
-        let email = userInfo.email;
-        let password = userInfo.password;
+    try {
+      userId = helpers.checkId(userId, "User ID");
+      dateId = helpers.checkId(dateId, "Date ID");
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
+    let user;
+    let date;
+    try {
+      user = await userFuncs.get(userId);
+      //GET DATE HERE
+    } catch (e) {
+      if (e === `No User with that ID (${id})`) {
+        return res.status(404).json({ error: e });
+      } else {
+        return res.status(500).json({ error: e });
+      }
+    }
+    let likedDates = user.likedDates;
 
-        try{
-            //VALIDATE FIELDS
-            firstName = helpers.checkName(firstName, "First Name");
-            lastName = helpers.checkName(lastName, "Last Name");
-            email = helpers.checkEmail(email, "Email");
-            password = helpers.checkPassword(password, "Password");
+    try {
+      if (likedDates.includes(dateId)) {
+        throw `User (${userId}) already liked that date (${dateId})`;
+      }
+    } catch (e) {
+      return res.status(400).json({ error: e });
+    }
 
-            const usersCollection = await users();
-            let userExists = await usersCollection.findOne({email: email});
-            if(userExists){
-                throw `User with that email (${email}) already exists!`
-            }
-
-        } catch(e){
-            return res.status(400).json({error: e});
-        }
-
-        try{
-            let newUser = await userFuncs.create(firstName,lastName,email,password);
-            return res.status(200).json(newUser);
-
-        } catch(e){
-            return res.status(500).json({error: e});
-        }
-    })
-
+    try {
+      let addInfo = await userFuncs.addLikedDate(userId, dateId);
+      return res.status(200).json(addInfo);
+    } catch (e) {
+        return res.status(500).json({ error: e });
+    }
+  })
+  .delete(async (req, res) => {});
 router
-    .route('/:id')
-    .get(async (req, res) => {
-        let id = req.params.id;
-
-        try{
-            id = helpers.checkId(id, "ID");
-        } catch (e){
-            return res.status(400).json({error: e});
-        }
-
-        try{
-            let user = await userFuncs.get(id);
-            return res.status(200).json(user);
-
-        } catch (e){
-            if (e === `No User with that ID (${id})`){
-                return res.status(404).json({error: e});
-            } else{
-                return res.status(500).json({error: e});
-            }
-        }
-    })
-    .patch(async (req, res) => {
-        let userInfo = req.body;
-        let id = req.params.id;
-
-        try{
-            id = helpers.checkId(id, "ID");
-        } catch (e){
-            return res.status(400).json({error: e});
-        }
-
-        const usersCollection = await users();
-        let user;
-
-        try{
-            user = await userFuncs.get(id);
-        } catch (e){
-            return res.status(404).json({error: e})
-        }
-
-        if (!userInfo || Object.keys(userInfo).length === 0){
-            return res.status(400).json({error: 'There are no fields in the request body'});
-        }
-
-        let firstName = userInfo.firstName;
-        let lastName = userInfo.lastName;
-        let email = userInfo.email;
-        let password = userInfo.password;
-
-        if (!firstName){
-            firstName = user.firstName;
-        }
-
-        if (!lastName){
-            lastName = user.lastName;
-        }
-
-        if (!email){
-            email = user.email;
-        }
-
-        if (!password){
-            password = user.password;
-        }
-
-        try{
-            firstName = helpers.checkName(firstName, "First Name");
-            lastName = helpers.checkName(lastName, "Last Name");
-            email = helpers.checkEmail(email, "Email");
-            password = helpers.checkPassword(password, "Password");
-
-        } catch (e) {
-            return res.status(400).json({error: e});
-        }
-
-        try {
-            let updatedUser = await userFuncs.update(id,firstName,lastName,email,password);
-            return res.status(200).json(updatedUser);
-        } catch (e) {
-            return res.status(500).json({error: e})
-        }
-    })
-
-
+  .route("/:userId/date/:dateId")
+  .post(async (req, res) => {})
+  .delete(async (req, res) => {});
