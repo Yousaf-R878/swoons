445cfca27aebbd903b2b0b637f13fde838f6a2e4
khalton55 <117873588+khalton55@users.noreply.github.com>
2023-12-17 15:20:14 -0500

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import configRoutes from './routes/index.js'
+import admin from "firebase-admin";
+admin.initializeApp({
+  credential: admin.credential.cert("snoopers-de724-firebase-adminsdk-qzswl-2b73a4d26c.json"),
+  databaseURL: 'snoopers-de724.firebaseapp.com'
+})
 const app = express();
 
 app.use(express.json());
+
+
 
 configRoutes(app);
 
