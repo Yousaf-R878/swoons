3929614d506a3b60a3255743340c9dd88e7ca247
khalton55 <117873588+khalton55@users.noreply.github.com>
2023-12-17 13:58:49 -0500

    Update seed.js

--- server/src/tasks/seed.js
+++ server/src/tasks/seed.js
@@ -7,21 +7,22 @@ const db = await dbConnection();
 
 let main = async () => {
     try{
-        let user1 = await userFuncs.create("Kyle","Halton","kyle@kyle.com","Password123!");
+        let user1 = await userFuncs.create("123abc567","Kyle","Halton","kyle@kyle.com","Password123!");
+        console.log(user1);
 
-        let date1 = await dateFuncs.createDate("Kyle's Date 1");
-        let addDate = await userFuncs.addDate(user1._id, date1._id);
+        // let date1 = await dateFuncs.createDate("Kyle's Date 1");
+        // let addDate = await userFuncs.addDate(user1._id, date1._id);
 
-        let date2 = await dateFuncs.createDate("Kyle's Date 2");
-        let addDate2 = await userFuncs.addDate(user1._id, date2._id);
+        // let date2 = await dateFuncs.createDate("Kyle's Date 2");
+        // let addDate2 = await userFuncs.addDate(user1._id, date2._id);
 
 
-        let user2 = await userFuncs.create("Michael", "Thomas", "michael@michael.com", "Password456!");
+        // let user2 = await userFuncs.create("Michael", "Thomas", "michael@michael.com", "Password456!");
 
-        let date3 = await dateFuncs.createDate("Michael's Date 1");
-        let addDate3 = await userFuncs.addDate(user2._id, date3._id);
+        // let date3 = await dateFuncs.createDate("Michael's Date 1");
+        // let addDate3 = await userFuncs.addDate(user2._id, date3._id);
 
-        let likedDate = await userFuncs.addLikedDate(user1._id, date3._id);
+        // let likedDate = await userFuncs.addLikedDate(user1._id, date3._id);
 
     } catch (e){
         console.log(e);
