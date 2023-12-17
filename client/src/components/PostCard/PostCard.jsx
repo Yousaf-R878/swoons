61af375a84408b4bfedb1caac8846b29360865a5
notalim <alimneedstowork@gmail.com>
2023-12-17 14:06:57 -0500

    restrusctured props

--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
@@ -15,10 +15,10 @@ import { Heart } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { MessagesSquare } from "lucide-react";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 
@@ -27,117 +27,89 @@ import ViewCardModal from "./ViewCardModal";
 import { useEffect, useState } from "react";
 import { set } from "react-hook-form";
 
-const PostCard = () => {
-  const works = [
-    {
-      artist: "Ornella Binni",
-      art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
-      title: "A Cozy Start",
-      description: "This hangout starts you off in a wonderful park, lorem ipsuming all over the place. It’s quite lorem ipsum. So much so, that the whole thing lorem ipsum’d on me and I lorem ipsum’d myself. Very great stuff!"
-    },
-    {
-      artist: "Tom Byrom",
-      art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
-      title: "A Cozy Middle",
-      description: "This hangout starts you off in a wonderful park, lorem ipsuming all over the place. It’s quite lorem ipsum. So much so, that the whole thing lorem ipsum’d on me and I lorem ipsum’d myself. Very great stuff!"
-
-    },
-    {
-      artist: "Vladimir Malyavko",
-      art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
-      title: "A Cozy End",
-      description: "This hangout starts you off in a wonderful park, lorem ipsuming all over the place. It’s quite lorem ipsum. So much so, that the whole thing lorem ipsum’d on me and I lorem ipsum’d myself. Very great stuff!"
-
-    },
-  ];
-  const cardInfo = {
-    name: "John Doe",
-    username: "@johnDoe",
-    title: "A Cozy Hangout",
-    badges: ["Badge"],
-    likes: 100,
-    comments: 10,
-  };
-  const [comments, setComments] = useState([]);
-  useEffect(() => {
-    setComments([
-      {
-        name: "John Doe",
-        username: "@johnDoe",
-        comment: "This is a comment!",
-        timestamp: "2023-12-15"
-      },
-      {
-        name: "Jane Doe",
-        username: "@janeDoe",
-        comment: "This is another comment!",
-        timestamp: "2023-12-16"
-      }
-    ]);
-  });
+const PostCard = ({ date }) => {
     return (
-    <Dialog>
-      <DialogTrigger asChild>
-            <Card className="w-[350px]">
-      <CardHeader>
-        <div className="flex items-center">
-          <Avatar className="w-10 h-10 mr-2">
-            <AvatarImage src="" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <span>
-            <CardTitle className="text-xl">John Doe</CardTitle>
-            <CardDescription className="text-sm">@johnDoe</CardDescription>
-          </span>
-        </div>
-        <Separator />
-      </CardHeader>
-      <CardContent>
-        <h2>A Cozy Hangout</h2>
-        <div className="flex flex-row">
-          <Star color="#FFB240" fill="#FFB240" size={25} />
-          <Star color="#FFB240" size={25} />
-          <Star color="#FFB240" size={25} />
-          <StarHalf color="#FFB240" fill="#FFB240" size={25} />
-        </div>
-        <Badge className="bg-palecyan my-2 h-5 text-xs text-gray-500">
-          Badge
-        </Badge>
-        <ScrollArea className="w-full h-40 whitespace-nowrap rounded-md border">
-          <div className="flex w-max space-x-4 p-4">
-            {works.map((artwork) => (
-              <figure key={artwork.artist} className="shrink-0">
-                <div className="overflow-hidden rounded-md">
-                  <img
-                    src={artwork.art}
-                    alt={`Photo by ${artwork.artist}`}
-                    className="h-28 w-28 object-cover"
-                  />
-                </div>
-                {/* <figcaption className="pt-2 text-xs text-muted-foreground">
-                  Photo by{" "}
-                  <span className="font-semibold text-foreground">
-                    {artwork.artist}
-                  </span>
-                </figcaption> */}
-              </figure>
-            ))}
-          </div>
-          <ScrollBar orientation="horizontal" />
-        </ScrollArea>
-      </CardContent>
-      <CardFooter className="">
-        <Heart size={25} color="#FFA39C" className="mr-2" />
-        <span>100</span>
-        {/* Maybe make comments blue? */}
-        <MessagesSquare size={25} color="gray" className="ml-2 mr-2" />
-        <span>10</span>
-      </CardFooter>
-    </Card>
-      </DialogTrigger>
-      <ViewCardModal works={works} cardInfo={cardInfo} comments={comments}/>
-    </Dialog>
-  );
+        <Dialog>
+            <DialogTrigger asChild>
+                <Card className="w-[350px]">
+                    <CardHeader>
+                        <div className="flex items-center">
+                            <Avatar className="w-10 h-10 mr-2">
+                                <AvatarImage src="" alt="@shadcn" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <span>
+                                <CardTitle className="text-xl">
+                                    {date.creator.firstName}{" "}
+                                    {date.creator.lastName}
+                                </CardTitle>
+                                <CardDescription className="text-sm">
+                                    {date.creator.username}
+                                </CardDescription>
+                            </span>
+                        </div>
+                        <Separator />
+                    </CardHeader>
+                    <CardContent>
+                        <h2>{date.title}</h2>
+                        <div className="flex flex-row">
+                            <Star color="#FFB240" fill="#FFB240" size={25} />
+                            <Star color="#FFB240" size={25} />
+                            <Star color="#FFB240" size={25} />
+                            <StarHalf
+                                color="#FFB240"
+                                fill="#FFB240"
+                                size={25}
+                            />
+                        </div>
+                        {date.tags.map((tag, index) => (
+                            <Badge
+                                key={index}
+                                className="bg-palecyan mx-1 my-2 h-5 text-xs text-gray-500"
+                            >
+                                {tag}
+                            </Badge>
+                        ))}
+                        <ScrollArea className="w-full h-40 whitespace-nowrap rounded-md border">
+                            <div className="flex w-max space-x-4 p-4">
+                                {/* {works.map((artwork) => (
+                                    <figure
+                                        key={artwork.artist}
+                                        className="shrink-0"
+                                    >
+                                        <div className="overflow-hidden rounded-md">
+                                            <img
+                                                src={artwork.art}
+                                                alt={`Photo by ${artwork.artist}`}
+                                                className="h-28 w-28 object-cover"
+                                            />
+                                        </div>
+                                    </figure>
+                                ))} */}
+                            </div>
+                            <ScrollBar orientation="horizontal" />
+                        </ScrollArea>
+                    </CardContent>
+                    <CardFooter className="">
+                        <Heart size={25} color="#FFA39C" className="mr-2" />
+                        <span>{date.likes}</span>
+                        {/* Maybe make comments blue? */}
+                        <MessagesSquare
+                            size={25}
+                            color="gray"
+                            className="ml-2 mr-2"
+                        />
+                        <span>{date.comments.length}</span>
+                    </CardFooter>
+                </Card>
+            </DialogTrigger>
+            {/* <ViewCardModal
+                // works={works}
+                // cardInfo={cardInfo}
+                comments={date.comments}
+            /> */}
+        </Dialog>
+    );
 };
 
 export default PostCard;
--- client/src/pages/Explore/Explore.jsx
+++ client/src/pages/Explore/Explore.jsx
@@ -7,108 +7,177 @@ import { X } from "lucide-react";
 import SearchIcon from "../../assets/icons/SearchIcon.jsx";
 
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select";
 
+const works = [
+    {
+        artist: "Ornella Binni",
+        art: "https://images.unsplash.com/photo-1465869185982-5a1a7522cbcb?auto=format&fit=crop&w=300&q=80",
+        title: "A Cozy Start",
+        description:
+            "This hangout starts you off in a wonderful park, lorem ipsuming all over the place. It’s quite lorem ipsum. So much so, that the whole thing lorem ipsum’d on me and I lorem ipsum’d myself. Very great stuff!",
+    },
+    {
+        artist: "Tom Byrom",
+        art: "https://images.unsplash.com/photo-1548516173-3cabfa4607e9?auto=format&fit=crop&w=300&q=80",
+        title: "A Cozy Middle",
+        description:
+            "This hangout starts you off in a wonderful park, lorem ipsuming all over the place. It’s quite lorem ipsum. So much so, that the whole thing lorem ipsum’d on me and I lorem ipsum’d myself. Very great stuff!",
+    },
+    {
+        artist: "Vladimir Malyavko",
+        art: "https://images.unsplash.com/photo-1494337480532-3725c85fd2ab?auto=format&fit=crop&w=300&q=80",
+        title: "A Cozy End",
+        description:
+            "This hangout starts you off in a wonderful park, lorem ipsuming all over the place. It’s quite lorem ipsum. So much so, that the whole thing lorem ipsum’d on me and I lorem ipsum’d myself. Very great stuff!",
+    },
+];
+
+const propData = {
+    _id: {
+        $oid: "657e5b2df5c884180b4cf492",
+    },
+    title: "Audience Thing",
+    tags: ["Adventurous", "Adventurous", "Cultural", "Relaxing", "Educational"],
+    events: [
+        {
+            name: "Whose",
+            location: "Port Susan",
+            description: "Something power total guy process ready nor.",
+            tripAdvisorLocationImages: [],
+            tripAdvisorLocationUrl: "",
+            tripAdvisorRating: "",
+            tripAdvisorRatingImage: "",
+            tripAdvisorLocationId: "",
+        },
+        {
+            name: "Pull",
+            location: "Diazhaven",
+            description: "Take share western kid.",
+            tripAdvisorLocationImages: [],
+            tripAdvisorLocationUrl: "",
+            tripAdvisorRating: "",
+            tripAdvisorRatingImage: "",
+            tripAdvisorLocationId: "",
+        },
+    ],
+    likes: 0,
+    comments: [],
+    creator: {
+        firstName: "Leonard",
+        lastName: "Guzman",
+        username: "vgriffin",
+        picture: "../public/default_profile_pic.jpg",
+    },
+};
+
+const dates = [propData];
+
 const Explore = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [badges, setBadges] = useState([]);
-  const [selectedSort, setSelectedSort] = useState("recent");
+    const [inputValue, setInputValue] = useState("");
+    const [badges, setBadges] = useState([]);
+    const [selectedSort, setSelectedSort] = useState("recent");
 
-  const hasSearch = badges.length > 0 ? true : false;
+    const hasSearch = badges.length > 0 ? true : false;
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
 
-  // * This handles ENTER PRESS
-  const handleInputKeyPress = (e) => {
-    if (
-      e.key === "Enter" &&
-      inputValue &&
-      inputValue.trim().length > 0 &&
-      badges.length < 20
-    ) {
-      const formattedInputValue =
-        inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
-      setBadges([...badges, formattedInputValue]);
-      setInputValue("");
+    // * This handles ENTER PRESS
+    const handleInputKeyPress = (e) => {
+        if (
+            e.key === "Enter" &&
+            inputValue &&
+            inputValue.trim().length > 0 &&
+            badges.length < 20
+        ) {
+            const formattedInputValue =
+                inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+            setBadges([...badges, formattedInputValue]);
+            setInputValue("");
+        }
+    };
+
+    const handleRemoveBadge = (indexToRemove) => {
+        setBadges(badges.filter((_, index) => index !== indexToRemove));
+    };
+
+    const handleSortChange = (e) => {
+        // TODO: this should handle sorting
+        setSelectedSort(e.target.value);
+    };
+
+    if (dates.length === 0) {
+        return <div>Loading...</div>;
     }
-  };
 
-  const handleRemoveBadge = (indexToRemove) => {
-    setBadges(badges.filter((_, index) => index !== indexToRemove));
-  };
-
-  const handleSortChange = (e) => {
-    // TODO: this should handle sorting
-    setSelectedSort(e.target.value);
-  };
-
-  return (
-    <>
-      <NavbarExplore />
-      <div className="container mx-auto p-4">
-        <div className="w-full max-w-lg mx-auto">
-          <div className="relative mb-2">
-            <Input
-              className="w-full text-lg pl-3 pr-10"
-              placeholder="Search for dates!"
-              value={inputValue}
-              onChange={handleInputChange}
-              onKeyPress={handleInputKeyPress}
-            />
-            <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
-              <SearchIcon className="w-5 h-5 text-gray-400" />
+    return (
+        <>
+            <NavbarExplore />
+            <div className="container mx-auto p-4">
+                <div className="w-full max-w-lg mx-auto">
+                    <div className="relative mb-2">
+                        <Input
+                            className="w-full text-lg pl-3 pr-10"
+                            placeholder="Search for dates!"
+                            value={inputValue}
+                            onChange={handleInputChange}
+                            onKeyPress={handleInputKeyPress}
+                        />
+                        <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+                            <SearchIcon className="w-5 h-5 text-gray-400" />
+                        </div>
+                    </div>
+                    <div className="flex flex-wrap gap-2">
+                        {badges.map((badge, index) => (
+                            <div
+                                key={index}
+                                className="flex items-center bg-palecyan text-sm py-1 px-2.5 rounded-full mr-1 text-gray-500"
+                            >
+                                {badge}
+                                <button
+                                    onClick={() => handleRemoveBadge(index)}
+                                    className="ml-1"
+                                >
+                                    <X size={17} />
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="flex justify-between items-center my-10">
+                    <h2 className="text-2xl font-semibold">
+                        {hasSearch ? "Found Results" : "Recent Activity"}
+                    </h2>
+                    <Select className="w-1/5">
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Sort by.." />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="recent">Recent</SelectItem>
+                            <SelectItem value="rating">Rating</SelectItem>
+                            <SelectItem value="recommended">
+                                Recommended
+                            </SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div className="flex justify-center">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20">
+                        {dates.map((date) => (
+                            <PostCard key={date._id} date={date} />
+                        ))}
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="flex flex-wrap gap-2">
-            {badges.map((badge, index) => (
-              <div
-                key={index}
-                className="flex items-center bg-palecyan text-sm py-1 px-2.5 rounded-full mr-1 text-gray-500"
-              >
-                {badge}
-                <button
-                  onClick={() => handleRemoveBadge(index)}
-                  className="ml-1"
-                >
-                  <X size={17} />
-                </button>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex justify-between items-center my-10">
-          <h2 className="text-2xl font-semibold">
-            {hasSearch ? "Found Results" : "Recent Activity"}
-          </h2>
-          <Select className="w-1/5">
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Sort by.." />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="recent">Recent</SelectItem>
-              <SelectItem value="rating">Rating</SelectItem>
-              <SelectItem value="recommended">Recommended</SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <div className="flex justify-center">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20">
-            <PostCard />
-            <PostCard />
-            <PostCard />
-            <PostCard />
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Explore;
