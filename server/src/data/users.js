7416c644aa0ff071a51ecb2257a9e21636e7af26
khalton55 <kyle@netrealm.com>
2023-12-19 20:29:43 -0500

    Update users.js

--- server/src/data/users.js
+++ server/src/data/users.js
@@ -276,3 +276,27 @@ export let removeDate = async (userId, dateId) => {
   removeInfo._id = removeInfo._id.toString();
   return removeInfo;
 };
+
+export let updatePhoto = async (id, url) => {
+  // id = helpers.checkId(id,"User's Id");
+  url = helpers.checkString(url, "Profile Picture URL");
+
+  const usersCollection = await users();
+  let user = get(id);
+
+  let newProfilePic = {
+      picture: url
+  }
+
+  let updateInfo = await usersCollection.findOneAndUpdate(
+      {_id: id}, 
+      {$set: newProfilePic},
+      {returnDocument: 'after'}
+  );
+
+  if (!updateInfo){
+      throw `Could not update User (${id})'s profile pic!`
+  }
+
+  return updateInfo;
+}
