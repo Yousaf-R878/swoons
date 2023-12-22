002f3870bea74d8adfeb847060230c9ce495237a
semphoenix <rodriguezs2021@gmail.com>
2023-12-21 23:48:34 -0500

    deploy

--- server/src/server.js
+++ server/src/server.js
@@ -14,6 +14,8 @@ app.use(
 app.use(express.json());
 // health check
 app.get("/", async function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   return res.status(200).json({
     ping: "pong",
   });
