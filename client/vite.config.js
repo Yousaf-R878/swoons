4d5de202b8892cad1158ae074a596e71ded8f839
semphoenix <rodriguezs2021@gmail.com>
2023-12-21 21:26:26 -0500

    more fixes

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./"),
     },
   },
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
 });
